377cdd20c492dd1e75ca348167d1acee6a4eceeb
luise <luisenriquevelezsantos@gmail.com>
2021-11-07 20:06:15 -0500

    Start

---
+++ app_Tech/views.py
@@ -0,0 +1,10 @@
+from django.http import request, response, HttpResponse, HttpResponseRedirect
+import json, math
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+def startApp(request):
+
+    return render(request,'appOne.html')
