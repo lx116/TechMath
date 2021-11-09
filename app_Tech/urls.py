b641cfb9cea2f0d5f2befe39088a33caa39c883f
luisenrique <luisenriquevelezsantos@gmail.com>
2021-11-09 17:01:57 -0500

    Rutas

--- app_Tech/urls.py
+++ app_Tech/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import startApp,AboutUs,PresentatioScreen,Documentation,interpolLineal
+from .views import startApp,AboutUs,PresentatioScreen,Documentation,interpolLineal,interpolacionLagrange,interpolacionNewton,interpolacionCuadratica
 urlpatterns = [
     path('inicio/',startApp,name='Inicio'),
     path('acercaDe/',AboutUs,name='Acerca de'),
     path('',PresentatioScreen,name='Home'),
     path('documentacion/',Documentation,name='Documentacion'),
-    path('formulaOne/',interpolLineal,name='linela1')
+    path('interpolacionLineal/',interpolLineal,name='linela1'),
+    path('interpolacionLagrange/',interpolacionLagrange,name='Lagrange'),
+    path('interpolacionNewton/',interpolacionNewton,name='Newton'),
+    path('interpolacionCuadratica/',interpolacionCuadratica,name='Cuadratica')
 ]
--- app_Tech/views.py
+++ app_Tech/views.py
@@ -1,9 +1,11 @@
 from django.http import request, response, HttpResponse, HttpResponseRedirect
 import json, math
+import numpy as np
+import matplotlib.pyplot as ptl
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from recursos.static.pythonCodes import interpolacionLineal
+
 
 def startApp(request):
 
@@ -24,6 +26,23 @@ def Documentation(request):
 
 def interpolLineal(request):
 
+    X = np.linspace(-np.pi, np.pi, 5)
+    Xexp = np.linspace(-np.pi, np.pi, 21)
+    Yexp = np.sin(Xexp)
 
-    interpolacionLineal.interpolLineal()
+    Y = np.interp(X, Xexp, Yexp)
+
+    ptl.plot(Xexp, Yexp)
+    ptl.plot(X, Y, 10)
+    ptl.show()
+
+def interpolacionCuadratica(request):
+    pass
+
+def interpolacionLagrange(request):
+    pass
+
+def interpolacionNewton(request):
+    pass
+
 
