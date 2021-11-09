511ed27f53fb83a6f3b96f3dbb46b7d8b862450d
luisenrique <luisenriquevelezsantos@gmail.com>
2021-11-09 16:39:44 -0500

    Views Update

--- app_Tech/urls.py
+++ app_Tech/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import startApp,AboutUs,PresentatioScreen,Documentation,interpolacionLineal
+from .views import startApp,AboutUs,PresentatioScreen,Documentation,interpolLineal
 urlpatterns = [
     path('inicio/',startApp,name='Inicio'),
     path('acercaDe/',AboutUs,name='Acerca de'),
     path('',PresentatioScreen,name='Home'),
     path('documentacion/',Documentation,name='Documentacion'),
-    path('formulaOne/',interpolacionLineal,name='linela1')
+    path('formulaOne/',interpolLineal,name='linela1')
 ]
