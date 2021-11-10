0a24697b31d28fe4eca8b65226d58edaa4d7b293
Raul <you@example.com>
2021-11-09 19:27:57 -0500

    Lagrange

--- app_Tech/views.py
+++ app_Tech/views.py
@@ -1,10 +1,6 @@
-from django.http import request, response, HttpResponse, HttpResponseRedirect
-import json, math
-import numpy as np
 import matplotlib.pyplot as ptl
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+import numpy as np
+from django.shortcuts import render
 
 
 def startApp(request):
@@ -31,7 +27,7 @@ def interpolLineal(request):
     Yexp = np.sin(Xexp)
 
     Y = np.interp(X, Xexp, Yexp)
-
+    print(Y)
     ptl.plot(Xexp, Yexp)
     ptl.plot(X, Y, 10)
     ptl.show()
@@ -40,7 +36,51 @@ def interpolacionCuadratica(request):
     pass
 
 def interpolacionLagrange(request):
-    pass
+    import numpy as np
+    import sympy as sym
+    import matplotlib.pyplot as plt
+
+    #Datos
+    xi = np.array([0, 0.2, 0.3, 0.4])
+    fi = np.array([1, 1.6, 1.7, 2.0])
+
+    #Procedimiento
+
+    n = len(xi)
+    x = sym.symbols("x")
+    polinomio = 0
+    for i in range(0, n, 1):
+        numerador = 1
+        denominador = 1
+
+    for j in range(0, n, 1):
+        if i != j:
+            numerador = numerador * (x - xi[j])
+            denominador = denominador * (xi[i] - xi[j])
+            termino = (numerador / denominador) * fi[i]
+            polinomio = polinomio + termino
+            polisimple = sym.expand(polinomio)
+            px = sym.lambdify(x, polinomio)
+
+    #Vectores_para_graficar
+
+    muestra = 51
+    a = np.min(xi)
+    b = np.max(xi)
+    p_xi = np.linspace(a, b, muestra)
+    pfi = px(p_xi)
+
+    #salida
+    print('Polinomio')
+    print(polinomio)
+    print('Polisimple:')
+    print(polisimple)
+
+    #Grafica
+    plt.plot(xi, fi, 'o')
+    plt.plot(p_xi, pfi)
+    plt.show()
+
 
 def interpolacionNewton(request):
     pass
