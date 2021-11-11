5271c886df14cfb9cb14e1fc29b47c9434f37e59
luisenrique <luisenriquevelezsantos@gmail.com>
2021-11-10 22:25:16 -0500

    Un rezo pa que ganemos el corte

--- app_Tech/views.py
+++ app_Tech/views.py
@@ -28,7 +28,7 @@ def funcioCentral(request):
 
     variablesX = data.get('X')
     variablesY = data.get('Y')
-    valorZ= float(data.get('Z'))
+    valorZ = float(data.get('Z'))
     print(variablesX)
     print(variablesY)
 
@@ -38,52 +38,150 @@ def funcioCentral(request):
     for var in variablesX:
         arrayX.append(float(var))
 
+    for var in variablesY:
+        arrayY.append(float(var))
 
-def interpolLineal(request):
+    print(arrayX)
+    print(arrayY)
+    print(valorZ)
 
-   #Aqui van los datos de los inputs
-    x = np.array([3, 5, 7])
-    y = np.array([10, 14, 22])
+    #interpolLineal(arrayX, arrayY, valorZ)
+    #interpolacionCuadratica(arrayX,arrayY,valorZ)
+    interpolacionLagrange(arrayX,arrayY,valorZ)
 
-   #Se interpola y "xi" es la x a buscar
+def interpolLineal(arrayX, arrayY, valorZ):
+    # Aqui van los datos de los inputs
+    x = np.array(arrayX)
+    y = np.array(arrayY)
+
+    # Se interpola y "xi" es la x a buscar
 
     polinomio = interpolate.interp1d(x, y)
 
-    xi = 4
+    xi = valorZ
     yi = polinomio(xi)
-
 
     print(yi)
 
-
-   #Se muestra la tabla
+    # Se muestra la tabla
     plt.plot(x, y, 'o:', xi, yi, 'sr')
     plt.grid()
     plt.title('Grafica', fontsize=16)
     plt.text(xi, yi, ' interpolacion ' + str(yi))
     plt.show()
 
-def interpolacionCuadratica(request):
+    return yi
+
+
+def interpolacionCuadratica(arrayX,arrayY,valorZ):
     # Aqui van los datos de los inputs
-    x = np.array([3, 5, 7])
-    y = np.array([10, 14, 22])
+    x = np.array(arrayX)
+    y = np.array(arrayY)
 
     cuadratico = interpolate.interp1d(x, y, kind='quadratic')
-    xi = 4
+    xi = valorZ
     ki = cuadratico(xi)
     print(ki)
 
-def interpolacionLagrange(request):
-    #Datos de la tabla
-    x = np.array([1, 4, 6, 8])
-    y = np.array([0, 1.386294, 1.791760, 2.079441])
+    return ki
 
-    #Calculo
+def interpolacionLagrange(arrayX,arrayY,valorZ):
+    # Datos de la tabla
+    x = np.array(arrayX)
+    y = np.array(arrayY)
+
+    # Calculo
     poly = interpolate.lagrange(x, y)
 
-    #Imprime - FALTA LA TABLA
+    # Imprime - FALTA LA TABLA
     print(poly)
 
 
 def interpolacionNewton(request):
- pass
+    # INGRESO , Datos de prueba
+    xi = np.array([3.2, 3.8, 4.2, 4.5])
+    fi = np.array([5.12, 6.42, 7.25, 6.85])
+
+    # PROCEDIMIENTO
+
+    # Tabla de Diferencias Divididas Avanzadas
+    titulo = ['i   ', 'xi  ', 'fi  ']
+    n = len(xi)
+    ki = np.arange(0, n, 1)
+    tabla = np.concatenate(([ki], [xi], [fi]), axis=0)
+    tabla = np.transpose(tabla)
+
+    # diferencias divididas vacia
+    dfinita = np.zeros(shape=(n, n), dtype=float)
+    tabla = np.concatenate((tabla, dfinita), axis=1)
+
+    # Calcula tabla, inicia en columna 3
+    [n, m] = np.shape(tabla)
+    diagonal = n - 1
+    j = 3
+    while (j < m):
+        # Añade título para cada columna
+        titulo.append('F[' + str(j - 2) + ']')
+
+        # cada fila de columna
+        i = 0
+        paso = j - 2  # inicia en 1
+        while (i < diagonal):
+            denominador = (xi[i + paso] - xi[i])
+            numerador = tabla[i + 1, j - 1] - tabla[i, j - 1]
+            tabla[i, j] = numerador / denominador
+            i = i + 1
+        diagonal = diagonal - 1
+        j = j + 1
+
+    # POLINOMIO con diferencias Divididas
+    # caso: puntos equidistantes en eje x
+    dDividida = tabla[0, 3:]
+    n = len(dfinita)
+
+    # expresión del polinomio con Sympy
+    x = sym.Symbol('x')
+    polinomio = fi[0]
+    for j in range(1, n, 1):
+        factor = dDividida[j - 1]
+        termino = 1
+        for k in range(0, j, 1):
+            termino = termino * (x - xi[k])
+        polinomio = polinomio + termino * factor
+
+    # simplifica multiplicando entre (x-xi)
+    polisimple = polinomio.expand()
+
+    # polinomio para evaluacion numérica
+    px = sym.lambdify(x, polisimple)
+
+    # Puntos para la gráfica
+    muestras = 101
+    a = np.min(xi)
+    b = np.max(xi)
+    pxi = np.linspace(a, b, muestras)
+    pfi = px(pxi)
+
+    # SALIDA
+    np.set_printoptions(precision=4)
+    print('Tabla Diferencia Dividida')
+    print([titulo])
+    print(tabla)
+    print('dDividida: ')
+    print(dDividida)
+    print('polinomio: ')
+    print(polinomio)
+    print('polinomio simplificado: ')
+    print(polisimple)
+
+    # Gráfica
+    plt.plot(xi, fi, 'o', label='Puntos')
+    ##for i in range(0,n,1):
+    ##plt.axvline(xi[i],ls='--', color='yellow')
+    plt.plot(pxi, pfi, label='Polinomio')
+    plt.legend()
+    plt.xlabel('xi')
+    plt.ylabel('fi')
+    plt.title('Diferencias Divididas - Newton')
+    plt.show()
+    pass
